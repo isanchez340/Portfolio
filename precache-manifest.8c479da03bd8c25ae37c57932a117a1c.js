48a0a8ae31dc7ec71b0a127add9c3f9f73a2c6dd
Irving <sanchez.irving006@gmail.com>
2020-01-23 01:05:46 -0700

    Updates

---
+++ precache-manifest.8c479da03bd8c25ae37c57932a117a1c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36988d0a7d6d28d7cca9a0bd0a58c01f",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "492276854ed6def6b419",
+    "url": "/Portfolio/static/css/main.3bf26f4d.chunk.css"
+  },
+  {
+    "revision": "9076cd372b4b64f20c2e",
+    "url": "/Portfolio/static/js/2.3a371958.chunk.js"
+  },
+  {
+    "revision": "e6edf5f5109ad7460f75a9f5716d868f",
+    "url": "/Portfolio/static/js/2.3a371958.chunk.js.LICENSE"
+  },
+  {
+    "revision": "492276854ed6def6b419",
+    "url": "/Portfolio/static/js/main.606f2b31.chunk.js"
+  },
+  {
+    "revision": "3ba21cbdd22395fc7838",
+    "url": "/Portfolio/static/js/runtime-main.09391d09.js"
+  },
+  {
+    "revision": "ebed3541b660867759748027577ddfff",
+    "url": "/Portfolio/static/media/avatar.ebed3541.jpg"
+  }
+]);
